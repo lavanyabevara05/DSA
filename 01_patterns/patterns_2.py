cd604acfb8f06f421dbd11c86305f4709cde125a
lavanyabevara05 <lavanyabevara1805@gmail.com>
2026-07-11 14:59:43 +0530

    Added one more Extra pattern

--- 01_patterns/patterns_2.py
+++ 01_patterns/patterns_2.py
@@ -200,13 +200,10 @@ class Solution:
             print()
 
         for i in range(n - 1, 0, -1):
-
             for j in range(i):
                 print("*", end="")
-
             for j in range(2 * (n - i)):
                 print(" ", end="")
-
             for j in range(i):
                 print("*", end="")
 
